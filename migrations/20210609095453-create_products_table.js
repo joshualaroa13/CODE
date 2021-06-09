f356c405a02c57c86c8eb66a55a6a2d1c0295059
natto777 <72873081+natto777@users.noreply.github.com>
2021-06-09 02:58:34 -0700

    Create 20210609095453-create_products_table.js

---
+++ migrations/20210609095453-create_products_table.js
@@ -0,0 +1,83 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     await queryInterface.createTable('products', { 
+      id:{
+        type: Sequelize.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+
+      code: {
+        type:Sequelize.STRING,
+        allowNull: false
+      },
+
+      productName: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      xs: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      
+      s: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      m: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      l: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      xl: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+
+      xl_2: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt:{
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt:{
+        type: Sequelize.DATE
+      },
+      deletedAt:{
+        type: Sequelize.DATE
+      }
+
+  
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('products');
+    
+  }
+};
